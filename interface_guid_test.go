309f8056df562751048b792c4fbca5a8327fad2e
Marc-Antoine Ruel <maruel@chromium.org>
2017-08-29 10:51:39 -0400

    Remove dependency on github.com/maruel/ut
    interfaceGUID now purely depends on stdlib

--- interface_guid_test.go
+++ interface_guid_test.go
@@ -8,8 +8,6 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
-
-	"github.com/maruel/ut"
 )
 
 func ExampleCalculate() {
@@ -25,8 +23,8 @@ func ExampleCalculate() {
 func TestCalculatePrivateName(t *testing.T) {
 	type value1 int
 	type value2 int
-	ut.AssertEqual(t, "26461de90bccabfde78889464de27c40b1a962fdc312a7b6284dd21593ffcd83", Calculate(reflect.TypeOf((*value1)(nil)).Elem()))
-	ut.AssertEqual(t, "26461de90bccabfde78889464de27c40b1a962fdc312a7b6284dd21593ffcd83", Calculate(reflect.TypeOf((*value2)(nil)).Elem()))
+	run(t, "26461de90bccabfde78889464de27c40b1a962fdc312a7b6284dd21593ffcd83", reflect.TypeOf((*value1)(nil)).Elem())
+	run(t, "26461de90bccabfde78889464de27c40b1a962fdc312a7b6284dd21593ffcd83", reflect.TypeOf((*value2)(nil)).Elem())
 }
 
 func TestCalculateFooBase(t *testing.T) {
@@ -38,7 +36,7 @@ func TestCalculateFooBase(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooMethodType(t *testing.T) {
@@ -50,7 +48,7 @@ func TestCalculateFooMethodType(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "7945944c9adf174cbac6e4b6bac219bb4eedd802818cbdc6477b6b3f7b9f9cb1", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "7945944c9adf174cbac6e4b6bac219bb4eedd802818cbdc6477b6b3f7b9f9cb1", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooMethodOrder(t *testing.T) {
@@ -62,7 +60,7 @@ func TestCalculateFooMethodOrder(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooInnerName(t *testing.T) {
@@ -74,7 +72,7 @@ func TestCalculateFooInnerName(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo2
 	}
-	ut.AssertEqual(t, "ac703259173d15ced210292885a05c6bd0d7d766d97a6fec32fe6e950d81593a", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "ac703259173d15ced210292885a05c6bd0d7d766d97a6fec32fe6e950d81593a", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooPrivate(t *testing.T) {
@@ -87,7 +85,7 @@ func TestCalculateFooPrivate(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "2afcbe99c3d2df8d2273723de6046a2922284a22bbe3ec91d2e312ea7b092fe5", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "2afcbe99c3d2df8d2273723de6046a2922284a22bbe3ec91d2e312ea7b092fe5", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooPrivateDifferentName(t *testing.T) {
@@ -100,7 +98,7 @@ func TestCalculateFooPrivateDifferentName(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "20b6295934efe4f25ead7ccf461e8894d99ee94a2f4e34e4200010c67aa2d621", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "20b6295934efe4f25ead7ccf461e8894d99ee94a2f4e34e4200010c67aa2d621", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooReturn(t *testing.T) {
@@ -112,7 +110,7 @@ func TestCalculateFooReturn(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "aa65c9ef02f86f231200d0e3ac25f642e18a97730967a8a24203454ae20f73eb", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "aa65c9ef02f86f231200d0e3ac25f642e18a97730967a8a24203454ae20f73eb", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateFooBarRenamed(t *testing.T) {
@@ -124,7 +122,7 @@ func TestCalculateFooBarRenamed(t *testing.T) {
 	type Bar interface {
 		GetFoo(int) Foo
 	}
-	ut.AssertEqual(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", Calculate(reflect.TypeOf((*Bar)(nil)).Elem()))
+	run(t, "18f7adec8708b2fd00336fb6d88911e465618c9429c823ca8f70c36f1794fcf1", reflect.TypeOf((*Bar)(nil)).Elem())
 }
 
 func TestCalculateMethodSelfReferencing(t *testing.T) {
@@ -132,13 +130,13 @@ func TestCalculateMethodSelfReferencing(t *testing.T) {
 	type Foo interface {
 		Bar() Foo
 	}
-	ut.AssertEqual(t, "891e72350ead590aaa4b8ff9b4c829ef6c9af819d5c041fbccfa8f54304e25ca", Calculate(reflect.TypeOf((*Foo)(nil)).Elem()))
+	run(t, "891e72350ead590aaa4b8ff9b4c829ef6c9af819d5c041fbccfa8f54304e25ca", reflect.TypeOf((*Foo)(nil)).Elem())
 }
 
 func TestCalculateTypeSelfReferencing(t *testing.T) {
 	// The type is self-referencing.
 	type Foo *Foo
-	ut.AssertEqual(t, "5a4f72702c70491229d3751c92ea8b403c4aacd7d9df54f79e27c439e8ee8f74", Calculate(reflect.TypeOf((*Foo)(nil)).Elem()))
+	run(t, "5a4f72702c70491229d3751c92ea8b403c4aacd7d9df54f79e27c439e8ee8f74", reflect.TypeOf((*Foo)(nil)).Elem())
 }
 
 // These types are used in TestCalculateMutuallyReferenced. Go doesn't support
@@ -153,5 +151,13 @@ type MutuallyReferenced2 interface {
 
 func TestCalculateMutuallyReferenced(t *testing.T) {
 	// The types are mutually referencing.
-	ut.AssertEqual(t, "109f2b3b0bcd2050177c557664fca8b7174e2bb6b6b688939f8b15d08cdd5ec1", Calculate(reflect.TypeOf((*MutuallyReferenced1)(nil)).Elem()))
+	run(t, "109f2b3b0bcd2050177c557664fca8b7174e2bb6b6b688939f8b15d08cdd5ec1", reflect.TypeOf((*MutuallyReferenced1)(nil)).Elem())
+}
+
+//
+
+func run(t *testing.T, expected string, v reflect.Type) {
+	if actual := Calculate(v); actual != expected {
+		t.Fatalf("Calculate(%v) = %s; expected %s", v, actual, expected)
+	}
 }
